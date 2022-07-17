40df6a192ec7dc15fbb9d8bc8d763a3975622532
Talal <talalfarrukh200@gmail.com>
2022-07-17 15:09:48 +0500

    Changed MongoDB password

--- server/index.js
+++ server/index.js
@@ -13,11 +13,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://talalblock999:Password123@cluster0.koymy.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://talalblock999:Potato123@cluster0.koymy.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true })
 .then( () => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) )
 .catch( (error) => console.log(error.message) )
-
 
